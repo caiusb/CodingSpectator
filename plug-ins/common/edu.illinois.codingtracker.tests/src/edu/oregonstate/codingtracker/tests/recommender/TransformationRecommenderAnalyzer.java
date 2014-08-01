580036ba431f0f34df9f46509fd3eb4da7b2cdbc
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-01 16:39:42 -0700

    Formatting

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
@@ -13,12 +13,14 @@ import edu.illinois.codingtracker.tests.analyzers.CSVProducingAnalyzer;
 import edu.illinois.codingtracker.tests.analyzers.ast.transformation.helpers.OperationFilePair;
 
 public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
-	
-	private final Map<Long, UnknownTransformationDescriptor> transformationKinds= new TreeMap<Long, UnknownTransformationDescriptor>();
 
-	private final Map<Long, OperationFilePair> atomicTransformations= new TreeMap<Long, OperationFilePair>();
-	
-	private final File transformationKindsFile = new File(Configuration.postprocessorRootFolderName, Configuration.TRANSFORMATION_KINDS_FILE);
+	private final Map<Long, UnknownTransformationDescriptor> transformationKinds = new TreeMap<Long, UnknownTransformationDescriptor>();
+
+	private final Map<Long, OperationFilePair> atomicTransformations = new TreeMap<Long, OperationFilePair>();
+
+	private final File transformationKindsFile = new File(
+			Configuration.postprocessorRootFolderName,
+			Configuration.TRANSFORMATION_KINDS_FILE);
 
 	@Override
 	protected String getTableHeader() {
@@ -41,16 +43,14 @@ public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
 	 * I am a processor that tries to recommend changes based on the things
 	 * coming in. For simplicity, I only look at the AST Changes. I already know
 	 * the common transformation patterns, because they are given to me by the
-	 * UnknownTransformatioAnalyzer. 
+	 * UnknownTransformatioAnalyzer.
 	 */
 	@Override
 	protected List<UserOperation> postprocess(List<UserOperation> userOperations) {
 		for (UserOperation userOperation : userOperations) {
-			if(!(userOperation instanceof ASTOperation))
+			if (!(userOperation instanceof ASTOperation))
 				continue;
-			
-			
-			
+
 		}
 		return userOperations;
 	}
@@ -59,5 +59,4 @@ public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
 	protected String getResultFilePostfix() {
 		return ".recommender";
 	}
-
 }
