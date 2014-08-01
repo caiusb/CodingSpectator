07e963db95ea12b09216422cde10a6648adf5e9f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-01 16:52:07 -0700

    Parsing the transformationKinds.csv file
    Into the correct data structure.

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
@@ -1,26 +1,76 @@
 package edu.oregonstate.codingtracker.tests.recommender;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.supercsv.cellprocessor.ParseLong;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvListReader;
+import org.supercsv.prefs.CsvPreference;
+
 import edu.illinois.codingtracker.helpers.Configuration;
 import edu.illinois.codingtracker.operations.UserOperation;
 import edu.illinois.codingtracker.operations.ast.ASTOperation;
+import edu.illinois.codingtracker.operations.ast.ASTOperationDescriptor.OperationKind;
 import edu.illinois.codingtracker.operations.ast.UnknownTransformationDescriptor;
 import edu.illinois.codingtracker.tests.analyzers.CSVProducingAnalyzer;
 import edu.illinois.codingtracker.tests.analyzers.ast.transformation.helpers.OperationFilePair;
 
 public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
 
-	private final Map<Long, UnknownTransformationDescriptor> transformationKinds = new TreeMap<Long, UnknownTransformationDescriptor>();
-
 	private final Map<Long, OperationFilePair> atomicTransformations = new TreeMap<Long, OperationFilePair>();
 
 	private final File transformationKindsFile = new File(
 			Configuration.postprocessorRootFolderName,
 			Configuration.TRANSFORMATION_KINDS_FILE);
+
+	/**
+	 * I parse the TransformationKinds file and return a new, populated map.
+	 */
+	private Map<Long, UnknownTransformationDescriptor> parseTransformationKindsFile() {
+		CsvListReader csvReader = null;
+		Map<Long, UnknownTransformationDescriptor> transformationKinds = new TreeMap<Long, UnknownTransformationDescriptor>();
+		try {
+			csvReader = new CsvListReader(new FileReader(
+					transformationKindsFile), CsvPreference.STANDARD_PREFERENCE);
+			List<Object> transformations;
+			csvReader.getHeader(true);
+			while ((transformations = csvReader.read(getCSVProcessors())) != null) {
+				transformationKinds.put(
+						(Long) transformations.get(0),
+						new UnknownTransformationDescriptor(OperationKind
+								.valueOf((String) transformations.get(1)),
+								(String) transformations.get(2),
+								(String) transformations.get(3),
+								(String) transformations.get(4)));
+			}
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		} finally {
+			try {
+				csvReader.close();
+			} catch (IOException e) {
+			}
+		}
+
+		return transformationKinds;
+	}
+
+	/**
+	 * I return the CSVProcessors to be used while parsing the
+	 * transformationKinds file.
+	 * 
+	 * @return an array with the processors
+	 */
+	private CellProcessor[] getCSVProcessors() {
+		return new CellProcessor[] { new ParseLong(), null, null, null, null };
+
+	}
 
 	@Override
 	protected String getTableHeader() {
@@ -47,6 +97,8 @@ public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
 	 */
 	@Override
 	protected List<UserOperation> postprocess(List<UserOperation> userOperations) {
+		Map<Long, UnknownTransformationDescriptor> unknownTransformationKinds = parseTransformationKindsFile();
+
 		for (UserOperation userOperation : userOperations) {
 			if (!(userOperation instanceof ASTOperation))
 				continue;
@@ -58,5 +110,10 @@ public class TransformationRecommenderAnalyzer extends CSVProducingAnalyzer {
 	@Override
 	protected String getResultFilePostfix() {
 		return ".recommender";
+	}
+
+	@Override
+	protected String getResult() {
+		return "";
 	}
 }
