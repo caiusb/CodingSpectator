4f2c12e9f17a9f447f8fef35b6c73aa5576ef5d7
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-03 15:59:23 -0700

    Removed unused and incorrect import

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/TransformationRecommenderAnalyzer.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
