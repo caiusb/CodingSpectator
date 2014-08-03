cde1470747d8d8506491a8c6da660e93ab35031b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-03 13:09:17 -0700

    Added a class to represent a candidate transformation

---
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/CandidateTransformation.java
@@ -0,0 +1,32 @@
+package edu.oregonstate.codingtracker.tests.recommender;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import edu.illinois.codingtracker.tests.analyzers.ast.transformation.Item;
+
+public class CandidateTransformation {
+
+	private Set<Item> itemSet;
+	private Set<Item> discoveredItems;
+
+	public CandidateTransformation(Set<Item> itemSet, Item firstItem) {
+		this.itemSet = itemSet;
+		discoveredItems = new TreeSet<Item>();
+		discoveredItems.add(firstItem);
+	}
+
+	public boolean continuesCandidate(Item item) {
+		if (discoveredItems.contains(item))
+			return true;
+		if (itemSet.contains(item))
+			return true;
+
+		return false;
+	}
+
+	public float getRanking() {
+		return ((float) discoveredItems.size()) / itemSet.size();
+	}
+
+}
