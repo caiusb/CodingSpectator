af4df9e7180f9c9682044638298ea784b87289c1
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-14 16:17:36 -0700

    Formatting

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/ItemSet.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/ItemSet.java
@@ -7,29 +7,29 @@ import java.util.TreeSet;
 import edu.illinois.codingtracker.tests.analyzers.ast.transformation.Item;
 
 public class ItemSet implements Iterable<Item> {
-	
+
 	private Set<Item> items = null;
 	private int size;
 	private int frequency;
-	
+
 	public ItemSet() {
 		items = new TreeSet<Item>();
 	}
-	
+
 	public ItemSet(Set<Item> items, int size, int frequency) {
 		this.items = items;
 		this.size = size;
 		this.frequency = frequency;
 	}
-	
+
 	public void addItem(Item item) {
 		items.add(item);
 	}
-	
+
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
+
 	public void setFrequency(int frequency) {
 		this.frequency = frequency;
 	}
