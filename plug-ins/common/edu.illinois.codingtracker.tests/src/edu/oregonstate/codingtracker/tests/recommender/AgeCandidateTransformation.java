ad1eed231b623b07b022a624d81fa1914e2bc26b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-25 09:42:17 -0700

    Added behaviour

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/AgeCandidateTransformation.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/AgeCandidateTransformation.java
@@ -4,18 +4,28 @@ import edu.illinois.codingtracker.tests.analyzers.ast.transformation.Item;
 
 public class AgeCandidateTransformation extends CandidateTransformation {
 	
-	public AgeCandidateTransformation(ItemSet itemSet, Item firstItem) {
+	public static final int DEFAULT_MAX_TIME = 5 * 60 * 1000; // 5 minutes = 5 * 60 seconds = 5 * 60 * 1000 milliseconds.
+	private final long startTime;
+	private final int maxTime;
+	
+	public AgeCandidateTransformation(ItemSet itemSet, Item firstItem, long startTime, int maxTime) {
 		super(itemSet, firstItem);
+		this.startTime = startTime;
+		this.maxTime = maxTime;
+	}
+	
+	public AgeCandidateTransformation(ItemSet itemSet, Item firstItem, long startTime) {
+		this(itemSet, firstItem, startTime, DEFAULT_MAX_TIME);
 	}
 
-	public static final int DEFAULT_MAX_TIME = 5 * 60 * 1000; // 5 minutes = 5 * 60 seconds = 5 * 60 * 1000 milliseconds. 
-	
 	@Override
 	public boolean continuesCandidate(ItemOccurance itemOccurance) {
 		if(super.continuesCandidate(itemOccurance))
 			return true;
 		
-		return false;
+		long time = itemOccurance.getTime();
+		
+		return (time - startTime) <= maxTime;
 	}
 
 }
