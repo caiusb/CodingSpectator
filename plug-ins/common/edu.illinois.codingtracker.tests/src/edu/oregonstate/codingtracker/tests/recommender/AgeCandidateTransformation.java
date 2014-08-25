60fa479a41b811a104f977338bd6bdf07f1a4666
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-25 09:30:46 -0700

    Stub for the AgeCandidateTransformation

---
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/oregonstate/codingtracker/tests/recommender/AgeCandidateTransformation.java
@@ -0,0 +1,21 @@
+package edu.oregonstate.codingtracker.tests.recommender;
+
+import edu.illinois.codingtracker.tests.analyzers.ast.transformation.Item;
+
+public class AgeCandidateTransformation extends CandidateTransformation {
+	
+	public AgeCandidateTransformation(ItemSet itemSet, Item firstItem) {
+		super(itemSet, firstItem);
+	}
+
+	public static final int DEFAULT_MAX_TIME = 5 * 60 * 1000; // 5 minutes = 5 * 60 seconds = 5 * 60 * 1000 milliseconds. 
+	
+	@Override
+	public boolean continuesCandidate(Item item) {
+		if(super.continuesCandidate(item))
+			return true;
+		
+		return false;
+	}
+
+}
