0157e636376eb6eb7067cbb0827e1d141e1a819a
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-27 16:37:00 -0700

    Added some behaviour to the class

--- plug-ins/common/edu.illinois.codingtracker.operations/src/edu/oregonstate/codingtracker/UpdatableTextChangeOperation.java
+++ plug-ins/common/edu.illinois.codingtracker.operations/src/edu/oregonstate/codingtracker/UpdatableTextChangeOperation.java
@@ -14,4 +14,25 @@ public class UpdatableTextChangeOperation {
 		this.actualLength = operation.getLength();
 		this.actualOffset = operation.getOffset();
 	}
+
+	public void updateInRegardTo(TextChangeOperation textChangeOperation) {
+		if (isAfter(textChangeOperation))
+			return;
+		
+		if (isBefore(textChangeOperation)) {
+			actualOffset += textChangeOperation.getLength();
+			return;
+		}
+		
+		// what to do if the thing actually happens in the middle? Can this be the case?
+		// I will ignore this case for now. I hope it does not bite me in the ass!
+	}
+
+	private boolean isBefore(TextChangeOperation textChangeOperation) {
+		return textChangeOperation.getOffset() < actualOffset;
+	}
+
+	private boolean isAfter(TextChangeOperation textChangeOperation) {
+		return textChangeOperation.getOffset() > (actualOffset + actualLength);
+	}
 }
