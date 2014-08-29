7ce0990e595ae322aab45f695dfaa2bc70b07884
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-29 15:44:13 -0700

    Added method to match existing edits to an...
    AST operations

--- plug-ins/common/edu.illinois.codingtracker.operations/src/edu/oregonstate/codingtracker/EditTransformationMapper.java
+++ plug-ins/common/edu.illinois.codingtracker.operations/src/edu/oregonstate/codingtracker/EditTransformationMapper.java
@@ -3,6 +3,8 @@ package edu.oregonstate.codingtracker;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+
 import edu.illinois.codingtracker.operations.textchanges.TextChangeOperation;
 
 public class EditTransformationMapper {
@@ -28,6 +30,18 @@ public class EditTransformationMapper {
 		}
 		unmachedOperations.add(new UpdatableTextChangeOperation(textChangeOperation));
 	}
+
+	public void matchEditsToAST(ASTNode node, long transformationID) {
+		int length = node.getLength();
+		int offset = node.getStartPosition();
+		
+		ArrayList<UpdatableTextChangeOperation> copyOfUnmachedOperations = makeCopyOfUnmachedOperations();
+		for (UpdatableTextChangeOperation operation : copyOfUnmachedOperations) {
+			if (operation.contains(offset,length)) {
+				unmachedOperations.remove(operation);
+				operation.getOperation().addToTransformation(transformationID);
+			}
+		}
 	}
 
 	private ArrayList<UpdatableTextChangeOperation> makeCopyOfUnmachedOperations() {
